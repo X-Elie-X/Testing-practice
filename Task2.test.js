3cd8f2f280f2225ea34318c8f9d97d8e26cccb40
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-08-17 21:39:35 +0200

    Create Task2.test.js

---
+++ Task2.test.js
@@ -0,0 +1,21 @@
+const reverse = require('./Task2');
+
+test('reverse string', () => {
+  expect(reverse('terminal')).toBe('lanimret');
+});
+
+test('reverse string', () => {
+  expect(reverse('output')).toBe('tuptuo');
+});
+
+test('reverse string', () => {
+  expect(reverse('terminal')).toBe('lanimret');
+});
+
+test('reverse string', () => {
+  expect(reverse('task')).toBe('ksat');
+});
+
+test('reverse string', () => {
+  expect(reverse('string')).toBe('gnirts');
+});
