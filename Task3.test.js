02250787c783072269992a26171f70883889dd2b
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-08-17 22:08:04 +0200

    Revert "Create Task3.test.js"
    This reverts commit 523c505b957818661b4a63046664ca55aaafc7dc.

--- Task3.test.js
+++
@@ -1,9 +0,0 @@
-const capitalize = require('./Task3');
-
-test('The first letter is Capital ', () => {
-  expect(capitalize('Problems')).toBe('P');
-});
-
-test('The first letter is Capital ', () => {
-  expect(capitalize('Github')).toBe('G');
-});
